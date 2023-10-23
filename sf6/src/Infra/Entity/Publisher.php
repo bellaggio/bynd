3fc89a4342fb0a7d2855690f6ac38a280abaab31
Rafael Costa <rcosta.bd@gmail.com>
2023-10-23 04:56:45 +0100

    GBI-001 added name parameter

--- sf6/src/Infra/Entity/Publisher.php
+++ sf6/src/Infra/Entity/Publisher.php
@@ -13,8 +13,28 @@ class Publisher
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(type: Types::TEXT, nullable: false)]
+    private string $name;
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Publisher
+     */
+    public function setName(string $name): Publisher
+    {
+        $this->name = $name;
+        return $this;
     }
 }
